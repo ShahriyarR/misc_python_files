6b79096055b072e78136e7ae3a88da74f8a5ddcf
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2014-09-03 18:47:43 +0500

    First File

---
+++ get_statistics.py
@@ -0,0 +1,36 @@
+import mysql.connector
+from termcolor import colored, cprint
+from tabulate import tabulate
+
+# MySQL Connection Parameters
+
+cnx = mysql.connector.connect(user='root', password='12345',
+                              host='localhost',
+                              database='lent_newsdb')
+table = cnx.cmd_statistics()
+
+dictlist = []
+
+column1 = colored('General Statistic Information', 'red', attrs=['reverse', 'dark'])
+column2 = colored('Values', 'red', attrs=['reverse', 'dark'])
+
+headers = [column1, column2]
+
+
+for key, value in table.iteritems():
+    temp = [colored(key,'green',attrs=['reverse']),value]
+    dictlist.append(temp)
+
+
+print tabulate(dictlist, headers, tablefmt="rst", numalign="right")
+
+print cnx.get_server_info()
+#print cnx.get_server_version()
+print cnx.get_sql_mode()
+print cnx.get_time_zone()
+print cnx.get_autocommit()
+print cnx.charset
+print cnx.collation
+
+
+cnx.close()
